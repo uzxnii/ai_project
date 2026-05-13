177b5237a937d4d630f4ffe929bcc1f37546126a
uzxnii <ijjing4@gmail.com>
2026-05-13 13:01:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title('나의 첫 웹사이트 만들기!')
 st.text_input('이름을 입력하세요')
-st.selectbox('좋아하는 음식을 선택하세요',['떡볶이','치킨','마라탕'])
+st.selectbox('좋아하는 음식을 선택하세요',['떡볶이','마라탕','요아정'])
 st.button('인사말 생성')
